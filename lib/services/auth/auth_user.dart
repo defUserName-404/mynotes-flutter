e165974659b64a9af2aa17738772dc0e347db0f4
Moshiur Rahman Nafi <moshiur.r.nafi@gmail.com>
2024-01-20 22:54:29 +0600

    Changed reusable widgets

--- lib/services/auth/auth_user.dart
+++ lib/services/auth/auth_user.dart
@@ -5,8 +5,8 @@ import 'package:flutter/foundation.dart';
 class AppAuthUser {
   final bool isEmailVerified;
 
-  const AppAuthUser(this.isEmailVerified);
+  const AppAuthUser({required this.isEmailVerified});
 
   factory AppAuthUser.fromFirebase(User user) =>
-      AppAuthUser(user.emailVerified);
+      AppAuthUser(isEmailVerified: user.emailVerified);
 }
