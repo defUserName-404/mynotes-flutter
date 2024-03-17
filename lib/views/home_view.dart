1d14b013a894bec9eff718d332e514c25323a52c
Moshiur Rahman Nafi <moshiur.r.nafi@gmail.com>
2024-03-18 02:35:06 +0600

    Fixed search bar icon not visible when on focus

--- lib/views/home_view.dart
+++ lib/views/home_view.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/scheduler.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:getwidget/components/loader/gf_loader.dart';
 import 'package:getwidget/types/gf_loader_type.dart';
@@ -28,7 +27,7 @@ class _HomeViewState extends State<HomeView>
   late final FocusNode _searchFocusNode;
   late final TextEditingController _searchController;
   final CloudStorageService _notesService = CloudStorageService();
-  late Iterable<CloudNote> _allNotes;
+  Iterable<CloudNote> _allNotes = [];
   late Iterable<CloudNote> _favoriteNotes;
 
   String get userId => AppAuthService.firebase().currentUser!.id;
@@ -55,15 +54,18 @@ class _HomeViewState extends State<HomeView>
     return SafeArea(
       child: Scaffold(
         appBar: _appBar(),
+        bottomSheet: _tabBar(),
         body: _body(),
         floatingActionButton: _fab(),
+        floatingActionButtonAnimator: FloatingActionButtonAnimator.scaling,
+        floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
       ),
     );
   }
 
   PreferredSizeWidget _appBar() {
     return AppBar(
-      elevation: 10,
+      elevation: 0,
       flexibleSpace: Container(
         height: AppBar().preferredSize.height,
         margin: const EdgeInsets.all(4.0),
@@ -73,39 +75,74 @@ class _HomeViewState extends State<HomeView>
           borderRadius: const BorderRadius.all(Radius.circular(4.0)),
         ),
       ),
-      bottom: _tabBar(),
       title: TextField(
-        controller: _searchController,
-        decoration: InputDecoration(
-          hintText: 'Search for notes',
-          border: InputBorder.none,
-          prefixIcon: AppIcon(icon: _isSearching ? Icons.close : Icons.search),
-        ),
-        autofocus: false,
-        focusNode: _searchFocusNode,
-        onTap: () {
-          setState(() {
-            _isSearching = !_isSearching;
-          });
-          if (!_isSearching) {
-            _searchController.text = '';
-            _searchFocusNode.unfocus();
-          }
-        },
-      ),
+          controller: _searchController,
+          decoration: InputDecoration(
+              hintText: 'Search for notes',
+              border: InputBorder.none,
+              prefixIcon: IconButton(
+                icon: _isSearching
+                    ? const Icon(Icons.close, color: CustomColors.primary)
+                    : const Icon(Icons.search, color: CustomColors.primary),
+                onPressed: () {
+                  if (_isSearching) {
+                    _searchController.text = '';
+                    _searchFocusNode.unfocus();
+                  }
+                  setState(() {
+                    _isSearching = !_isSearching;
+                  });
+                },
+              ),
+              suffixIcon: _isSearching
+                  ? IconButton(
+                      onPressed: () {
+                        _onTypingAndSearchButtonOrSubmitPressed(
+                            _searchController.text);
+                        setState(() {
+                          _isSearching = false;
+                        });
+                      },
+                      icon:
+                          const Icon(Icons.search, color: CustomColors.primary))
+                  : null),
+          autofocus: false,
+          focusNode: _searchFocusNode,
+          onTap: () {
+            if (!_isSearching) {
+              setState(() {
+                _isSearching = true;
+              });
+            }
+          },
+          onChanged: (searchText) {
+            _onTypingAndSearchButtonOrSubmitPressed(searchText);
+          },
+          onSubmitted: (searchText) {
+            _onTypingAndSearchButtonOrSubmitPressed(searchText);
+            setState(() {
+              _isSearching = false;
+            });
+          }),
       actions: [
         Visibility(
           visible: !_isSearching,
           maintainState: true,
           maintainAnimation: true,
-          child: IconButton(
-              icon: const AppIcon(icon: Icons.account_circle),
-              onPressed: () => context
-                  .read<AppAuthBloc>()
-                  .add(const AppAuthEventAccountSettings())),
+          child: Padding(
+            padding: const EdgeInsets.only(right: 16.0),
+            child: IconButton(
+                icon: const AppIcon(icon: Icons.account_circle),
+                onPressed: () => context
+                    .read<AppAuthBloc>()
+                    .add(const AppAuthEventAccountSettings())),
+          ),
         ),
       ],
     );
+  }
+
+  void _onTypingAndSearchButtonOrSubmitPressed(String searchText) {
   }
 
   Widget _body() {
@@ -141,18 +178,22 @@ class _HomeViewState extends State<HomeView>
     );
   }
 
-  PreferredSizeWidget _tabBar() {
+  Widget _tabBar() {
     return TabBar(
       indicatorSize: TabBarIndicatorSize.tab,
+      indicatorWeight: 8,
       indicatorColor: CustomColors.primary,
+      labelColor: CustomColors.primary,
+      enableFeedback: true,
+      indicatorPadding: const EdgeInsets.symmetric(horizontal: 4, vertical: 2),
       tabs: const [
         Tab(
           text: 'All Notes',
-          icon: AppIcon(icon: Icons.home),
+          icon: Icon(Icons.home),
         ),
         Tab(
           text: 'Favorite Notes',
-          icon: AppIcon(icon: Icons.favorite),
+          icon: Icon(Icons.favorite),
         )
       ],
       controller: _tabController,
@@ -162,13 +203,9 @@ class _HomeViewState extends State<HomeView>
   Widget _fab() {
     return FloatingActionButton(
       backgroundColor: CustomColors.primary,
-      onPressed: () {
-        SchedulerBinding.instance.addPostFrameCallback((timeStamp) {
-          Navigator.of(context).pushNamed(noteEditorNewNoteRoute);
-        });
-      },
+      onPressed: () => Navigator.of(context).pushNamed(noteEditorNewNoteRoute),
       tooltip: 'Add a new note',
-      child: const Icon(Icons.add_box),
+      child: const Icon(Icons.add_box, color: CustomColors.onPrimary),
     );
   }
 }
